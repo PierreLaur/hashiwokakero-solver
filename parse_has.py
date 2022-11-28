e25b031aae2802741900a36ec91f903e969d67bb
Pierre <pierre.laur31@gmail.com>
2022-11-28 12:01:10 +0100

    started implementing constraints

--- parse_has.py
+++ parse_has.py
@@ -1,12 +1,37 @@
 import sys
+class HashiGrid :
+    def __init__(self, width, height, n_islands) -> None:
+        self.width = width
+        self.height = height
 
-count = 0
-file = sys.argv[1]
-grid = []
-with open(file) as f :
-    width, height, n_islands = f.readline().split()
-    for line in f :
-        grid.append(list(map(lambda x : int(x), line.strip().split())))
+        self.n_islands = n_islands
 
-print(f'Width : {width} \nHeight : {height} \nNumber of islands : {n_islands} \n\n The grid : ')
-print(grid)
+        self.island_coordinates = []
+        self.digits = []
+
+    def fill_grid(self,grid) :
+        for i,line in enumerate(grid) :
+            for j,square in enumerate(line) :
+                if square != 0 :
+                    self.island_coordinates.append(
+                        (i,j)
+                    )
+                    self.digits.append(square)
+
+    def print_grid (self) :
+        for coords, d in zip(self.island_coordinates, self.digits) :
+            print(f'Island {coords}    with n_bridges = {d}')
+
+def read_has_file(file) :
+    count = 0
+    file = sys.argv[1]
+    grid = []
+    with open(file) as f :
+        width, height, n_islands = f.readline().split()
+        for line in f :
+            grid.append(list(map(lambda x : int(x), line.strip().split())))
+
+    h_grid = HashiGrid(width, height, int(n_islands))
+    h_grid.fill_grid(grid)
+    
+    return h_grid
--- test_cpsolver.py
+++ test_cpsolver.py
@@ -10,29 +10,54 @@
 
 from ortools.sat.python import cp_model
 
+import parse_has
+import sys
+
+def adjacent_vertices(coords, vertices) :
+    adj = []
+    for vertice in vertices :
+        if vertice[0] == coords[0] or vertice[1] == coords[1] :
+            adj.append((island[0],island[1]))
+    return adj
+
 def main() :
+
+    h_grid = parse_has.read_has_file(sys.argv[1])
+
     model = cp_model.CpModel()
     if not model :
         return
 
-    num_vals = 3
-    x = model.NewIntVar(0, num_vals - 1, 'x')
-    y = model.NewIntVar(0, num_vals - 1, 'y')
-    z = model.NewIntVar(0, num_vals - 1, 'z')
+    # declaring the variables
+    x_vars = {}
+    y_vars = {}
 
-    model.Add(x != y)
+    for i in range(h_grid.n_islands) :
+        for j in range(i+1, h_grid.n_islands) :
+            x_vars[(i,j)] = model.NewIntVar(0,2,'x_'+str(i)+"_"+str(j))
+            y_vars[(i,j)] = model.NewBoolVar('y_'+str(i)+"_"+str(j))
 
-
+    # constraints
+    for k in range(h_grid.n_islands) :
+        vars_to_sum = []
+        for i, (a,b) in enumerate(h_grid.island_coordinates[:k]) :
+            if h_grid.island_coordinates[k][0] == a or h_grid.island_coordinates[k][1] == b :
+                vars_to_sum.append(x_vars[(i,k)])
+        for j,(a,b) in enumerate(h_grid.island_coordinates[k+1:]) :
+            if h_grid.island_coordinates[k][0] == a or h_grid.island_coordinates[k][1] == b :            
+                vars_to_sum.append(x_vars[(k,j+k+1)])
+        model.Add(sum(vars_to_sum) == 1)
     solver = cp_model.CpSolver()
     status = solver.Solve(model)
 
+    # print the result
     if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
-        print('x = %i' % solver.Value(x))
-        print('y = %i' % solver.Value(y))
-        print('z = %i' % solver.Value(z))
+        for i in x_vars.keys() :
+            if solver.Value(x_vars[i]) > 0 :
+                print(f"{i} {solver.Value(x_vars[i])} bridges")
+
     else:
         print('No solution found.')
 
 if __name__ == '__main__':
-
     main()
